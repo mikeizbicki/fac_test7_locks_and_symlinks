9056788a883d0c8c42acb3fe79a94e8af4fa7f9a
Mike Izbicki <mike@izbicki.me>
2026-05-20 10:48:48 -0700

    edit tests

--- run_test.sh
+++ run_test.sh
@@ -1,34 +1,11 @@
 #!/bin/sh
 
-set -ex
-alias fac='python3 -m fac --auto_commit=False'
+# the following commands test that changes correctly propagate through
+# symlinks and locking/unlocking files
 
-# The idea of this test script is that we will run a series of build commands
-# and check to see if the files that have been created match the files that should have been created.
-# This function performs the actual check to see if the files match and will be called by the test cases below.
-dotest() {
-    # NOTE:
-    # The testing procedure is inspired by the standard for postgresql test scripts.
-    # This function should be called with a single parameter,
-    # which is the name of the test case.
-    # The ground truth files that should exist will be stored in the `.expected` folder,
-    # and the files that actually exist after the run of these tests will be stored in the `.results` folder.
-    # The `.results` folder should never be added to the git repo.
-    # We use hidden folder names (prefixed with the dot) so that they do not get tracked with the ls -R command.
-    mkdir -p .results
-    #files-to-prompt --ignore fac.yaml --ignore *.sh . > .results/"$1" 
-    # files-to-prompt outputs the contents of all files (recursively) to stdout;
-    # we cannot use the command above, however, because files-to-prompt is non-deterministic in its output order;
-    # we need to combine find + sort to get a deterministic ordering of the files
-    files-to-prompt $(find . -type f -not -path "*/.*" -type f -not -name "*.sh" -not -name "*.yaml" | sort) > .results/"$1"
-    diff .results/"$1" .expected/"$1"
-}
-
-# This function cleans all files in the repo except those in the results folder.
-# It is useful for writing tests that build from scratch.
-clean_repo() {
-    git clean -fd -e .results/
-}
+################################################################################
+# boring framework code
+################################################################################
 
 # The checks will all fail if there are uncommitted files in the repo.
 # We therefore ensure there are no uncommitted files before performing the tests.
@@ -38,32 +15,53 @@ if ! [ -z "$(git status --porcelain)" ]; then
     exit 1
 fi
 
-# the following commands test that changes correctly propagate through
-# symlinks and locking/unlocking files
-# tests that changes to 
-clean_repo
-fac 'test/f'
-dotest checkpoint1
+dotest() {
+    mkdir -p .results
+    cat > .results/"$1"
+    diff .results/"$1" .expected/"$1"
+}
 
-echo 'update' > test/b
-fac 'test/f'
-dotest checkpoint2
+# set default fac parameters if none given
+[ $# -eq 0 ] && set -- --auto_commit=False
 
-fac --lock test/c
-echo 'update again' > test/b
-fac 'test/f'
-dotest checkpoint3
+set -ex
 
-echo 'update third' > test/c
-fac 'test/f'
-dotest checkpoint4
+################################################################################
+# tests start here
+################################################################################
 
-rm test/d
-echo 'update fourth' > test/d
-fac 'test/f'
-dotest checkpoint5
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint1
 
-fac --unlock test/c
-echo 'unlock' > test/a
-fac 'test/f'
-dotest checkpoint6
+echo 'update' > build/b
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint2
+
+fac "$@" --lock build/c
+echo 'update again' > build/b
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint3
+
+echo 'update third' > build/c
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint4
+
+rm build/d
+echo 'update fourth' > build/d
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint5
+
+echo 'unlock' > build/a
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint6
+# FIXME:
+# the test above is "morally" broken;
+# the build to build/f triggers a rebuild of build/b even though
+# the intermediate dependency build/c is locked;
+# preventing builds from traversing locks requires some heavy re-engineering
+
+fac "$@" --unlock build/c
+fac "$@" 'build/f'
+files-to-prompt build | dotest checkpoint7
+
+git clean -fd
